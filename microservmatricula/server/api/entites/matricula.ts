44553a841f7cc0d00a1385c8a1f92f0403c019a5
ZeferinoUniville <104472203+ZeferinoUniville@users.noreply.github.com>
2023-11-17 19:24:05 +0000

    Criando tabela no cosmos

---
+++ microservmatricula/server/api/entites/matricula.ts
@@ -0,0 +1,12 @@
+import { LogFn } from "pino";
+
+export interface Aluno{
+    id: string;
+    Matricula: string;
+    Turma: string;
+    Nome: string;
+    Sobrenome: string;
+    cpf: string;
+    Email: string;
+    Phone: string;
+}
